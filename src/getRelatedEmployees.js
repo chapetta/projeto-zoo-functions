d149d00bbf7081182d5ef662972fbf27eb0826fd
chapetta <cha.petta@hotmail.com>
2023-05-21 15:45:53 -0300

    Done: requisito 4 - implemente a funçao getRelatedEmployess

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,16 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  return data.employees.some((item) => item.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId)) {
+    const managers = data.employees
+      .filter((item) => item.managers.includes(managerId));
+    return managers.map((person) => `${person.firstName} ${person.lastName}`);
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
 
 module.exports = { isManager, getRelatedEmployees };
