7a6c9b17e4b3882ebb7cb3ec137cfffd23d4921f
chapetta <cha.petta@hotmail.com>
2023-05-21 16:48:54 -0300

    Done: requisito 5 - implemente a funçao count Animals

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,21 @@
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
-}
+const { species } = data;
 
+function countAnimals(animal) {
+  console.log(animal);
+  const allSpecies = {};
+  if (!animal) {
+    species.forEach((item) => {
+      allSpecies[item.name] = item.residents.length;
+    });
+    return allSpecies;
+  }
+  if (!animal.sex) {
+    return species.find((item) => item.name === animal.specie).residents.length;
+  }
+  return species
+    .find((item) => item.name === animal.specie).residents
+    .filter((e) => e.sex === animal.sex).length;
+}
 module.exports = countAnimals;
