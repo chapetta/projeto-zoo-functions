9a82ed4827ac4b3975a0f2c0c3441b8416597133
chapetta <cha.petta@hotmail.com>
2023-05-23 21:50:45 -0300

    Done: requisito 10 - implemente a funaçao getOldestFromFirstSpecies

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
 
+const { employees, species } = data;
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const responsiblePerson = employees.find((person) => person.id === id);
+  const firstSpecie = responsiblePerson.responsibleFor[0];
+  const residentsOfSpecie = species.find((animal) => animal.id === firstSpecie).residents;
+  const sortedAniaml = residentsOfSpecie.sort((a, b) => a.age - b.age);
+  const result = Object.values(sortedAniaml[sortedAniaml.length - 1]);
+
+  return result;
 }
 
 module.exports = getOldestFromFirstSpecies;
