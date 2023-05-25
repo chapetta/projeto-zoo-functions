0bbe22c9ba5acf63dfc09ca01baac6ec83207a54
chapetta <cha.petta@hotmail.com>
2023-05-25 17:15:55 -0300

    Done: Requisito 14 - implemente a funçao getAnimalMap

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,21 @@
 const data = require('../data/zoo_data');
 
-function getAnimalMap(options) {
-  // seu código aqui
+function getAnimalMap(options = {}) {
+  const { includeNames = false, sorted = false, sex } = options;
+  const animalMap = { NE: [], NW: [], SE: [], SW: [] };
+  data.species.forEach((species) => {
+    const { name, location, residents } = species;
+    const filteredResidents = sex ? residents
+      .filter((resident) => resident.sex === sex) : residents;
+    const animalNames = filteredResidents.map((resident) => resident.name);
+    if (includeNames) {
+      const animalObj = { [name]: sorted ? animalNames.sort() : animalNames };
+      animalMap[location].push(animalObj);
+    } else {
+      animalMap[location].push(name);
+    }
+  });
+  return animalMap;
 }
 
 module.exports = getAnimalMap;
